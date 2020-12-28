b5123c682dfdcd86d08e00b75c2644e3bf955812
Jonas Myhr Refseth <jonas@oisann.net>
2020-12-28 20:42:36 +0100

    Implement main concept

--- main.go
+++ main.go
@@ -3,27 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"golang.zx2c4.com/wireguard/wgctrl"
 )
 
+var wg *wgctrl.Client
+var lastIP string
+
+func doEvery(d time.Duration, f func()) {
+	for range time.Tick(d) {
+		f()
+	}
+}
+
 func main() {
-	wg, err := wgctrl.New()
+	var err error
+
+	// TODO: Check DNS record to get the current "lastIP"
+	lastIP = "N/A"
+
+	wg, err = wgctrl.New()
 	if err != nil {
 		log.Fatalf("Unable to create client: %e", err)
 	}
+	check()
+	doEvery(2*time.Second, check)
+}
+
+func check() {
 	connections, err := wg.Devices()
 	if err != nil {
 		log.Fatalf("Unable to get connections: %e", err)
 	}
 	for _, con := range connections {
 		for _, peers := range con.Peers {
+			key := peers.PublicKey.String()
+			if key != "TEMTZpKBxQ26vzk/xs1lXyiwve8nQg3vhj6MlBP06l0=" {
+				continue
+			}
 			endpoint := peers.Endpoint
 			if endpoint != nil {
-				fmt.Println(endpoint)
+				ip := endpoint.IP
+				detectChange(ip.String())
 			} else {
 				fmt.Println("No endpoint in connection")
 			}
 		}
 	}
+}
+
+func detectChange(ip string) {
+	if lastIP != ip {
+		fmt.Printf("New IP detected! %s -> %s\n", lastIP, ip)
+	}
+	lastIP = ip
 }
